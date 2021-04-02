1c80a0be7bb79f81a5fab5ac1298dab4bd037926
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-02 10:44:28 +0300

    feat: complete task

--- Lesson02/index.js
+++ Lesson02/index.js
@@ -1,8 +1,22 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-alert('Задание 2');
-console.log('Задание 2');
+let money = 1200;
+let income = 'Подработка';
+let addExpenses = 'Телефон, интернет, топливо';
+let deposit = false;
+let mission = 50000;
+let period = 6;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+
+console.log(`Цель заработать за ${period} месяцев ${mission}$`);
+
+const arr = addExpenses.toLowerCase().split(', ');
+console.log(arr);
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+
+
