f3c92f527461015a86357ad65bbeb26be598b91f
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-05 09:21:40 +0300

    init: start task

---
+++ Lesson03/index.js
@@ -0,0 +1,19 @@
+let money = 1200;
+let income = 'Подработка';
+let addExpenses = 'Телефон, интернет, топливо';
+let deposit = false;
+let mission = 50000;
+let period = 6;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+
+console.log(`Цель заработать за ${period} месяцев ${mission}$`);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
