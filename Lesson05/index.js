9b1876cee48789ea225a336639b7ce4f0eb0a798
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-07 13:08:36 +0300

    fix: arguments naming

--- Lesson05/index.js
+++ Lesson05/index.js
@@ -26,10 +26,10 @@ showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-function getExpensesMonth(a) {
+function getExpensesMonth(monthExpensesAmount) {
     let sum = 0;
     for (let i = 0; i < 2; i++) {
-        a[i] = prompt('Введите обязательную статью расходов', 'еда');
+        monthExpensesAmount[i] = prompt('Введите обязательную статью расходов', 'еда');
         do {
             sum += +prompt('Во сколько это обойдется?', 150);
         }
@@ -42,16 +42,16 @@ console.log(`Расходы за месяц составят: ${expensesAmount}`
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-function getAccumulatedMonth(a) {
-    return a - expensesAmount;
+function getAccumulatedMonth(income) {
+    return income - expensesAmount;
 }
 
 let accumulatedMonth = getAccumulatedMonth(money);
 
-function getTargetMonth(a, b) {
-    if ((a / b) >= 0) {
-        console.log(`Цель будет достигнута за ${Math.ceil(a / b)} месяцев(-а)`);
-    } else if ((a / b) <= 0) {
+function getTargetMonth(target, monthAccumulate) {
+    if ((target / monthAccumulate) >= 0) {
+        console.log(`Цель будет достигнута за ${Math.ceil(target / monthAccumulate)} месяцев(-а)`);
+    } else if ((target / monthAccumulate) <= 0) {
         console.log(`Цель не будет достигнута`);
     }
 }
@@ -60,12 +60,12 @@ getTargetMonth(mission, accumulatedMonth);
 let budgetDay = Math.floor(accumulatedMonth / 30);
 console.log(`Бюджет на день ${budgetDay}`);
 
-let getStatusIncome = function (a) {
-    if (a >= 1200) {
+let getStatusIncome = function (dayExpenses) {
+    if (dayExpenses >= 1200) {
         return 'У вас высокий уровень дохода'
-    } else if (a >= 600 && a < 1200) {
+    } else if (dayExpenses >= 600 && dayExpenses < 1200) {
        return 'У вас средний уровень дохода'
-    } else if (a >= 0 && a < 600) {
+    } else if (dayExpenses >= 0 && dayExpenses < 600) {
         return 'К сожалению у вас уровень дохода ниже среднего. Парам-парам, пам...'
     } else {
         return 'Что то пошло не так'
