c838309f0971cc2e79cd7d4b7475fb91b249c0bd
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-21 16:17:48 +0300

    init: start lesson

---
+++ Lesson18/index.js
@@ -0,0 +1,47 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    
+    function addZero(x) {
+        return (parseInt(x, 10) < 10 && parseInt(x, 10) >= 0 ? '0' : '') + x;
+    }
+
+
+    // Timer
+    function counterTimer(deadline) {
+        let timerHours = document.querySelector('#timer-hours');
+        let timerMinutes = document.querySelector('#timer-minutes');
+        let timerSeconds = document.querySelector('#timer-seconds');
+        let timerDays = document.querySelector('#timer-days');
+        let timerNumbers = document.querySelectorAll('.timer-numbers');
+
+
+        function getTimeRemaining (){
+            let dateStop = new Date(deadline).getTime();
+            let dateNow = new Date().getTime();
+            let timeRemaining = (dateStop - dateNow) / 1000;
+            let seconds = Math.floor(timeRemaining % 60);
+            let minutes = Math.floor((timeRemaining / 60) % 60);
+            let hours = Math.floor((timeRemaining / 3600) % 24);
+            let days = Math.floor(timeRemaining / 3600 / 24);
+            return {timeRemaining, hours, minutes, seconds, days}
+        }
+        function updateClock() {
+            let timer = getTimeRemaining();
+            if (timer.seconds < 0) {
+                timerNumbers.forEach(num => num.style.color = 'red');
+                timerHours.textContent = '00';
+                timerMinutes.textContent = '00';
+                timerSeconds.textContent = '00';
+                timerDays.textContent = '0';
+            } else {        
+                timerHours.textContent = addZero(timer.hours);
+                timerMinutes.textContent = addZero(timer.minutes);
+                timerSeconds.textContent = addZero(timer.seconds);
+                timerDays.textContent = timer.days;
+            }
+        }
+        updateClock();
+        setInterval(updateClock, 1000);
+    }
+    counterTimer('01 may 2021');
+});
